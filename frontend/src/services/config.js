cc9dd5e1783a699050a9cd53994afb6e99262284
Silence <youcankeepsilence@gmail.com>
2019-10-23 18:19:47 +0300

    prepared to release at 10.31

--- frontend/src/services/config.js
+++ frontend/src/services/config.js
@@ -1,4 +1,4 @@
 export default {
   jiraUrlPrefix: 'https://jira.crpt.ru',
-  proxyAddress: 'http://127.0.0.1:3000' //  'http://192.168.10.31'
+  proxyAddress: 'http://192.168.10.31:3005' //  'http://192.168.10.31'
 }
